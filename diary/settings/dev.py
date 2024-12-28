c5aea5ef1b201ebd13321676c5bba521f4bb70c0
Aman Pandey <aman2001mi@gmail.com>
2024-12-28 13:49:09 +0530

    base wagtail setup

---
+++ diary/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-572g(2jdxzwsub-0#gjmj&to^wp@a7wdy4vqqob*7^#!caw@yv"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
